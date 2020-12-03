69c08c941e0d6bcb6d4af72049fd27860f446fad
akshay-narkar <akshaynarkar1295@gmail.com>
2020-12-03 20:23:11 +0530

    Add spacing to i/p and o/p

--- bin/main.rb
+++ bin/main.rb
@@ -6,27 +6,19 @@ require_relative '../lib/game_logic'
 play_again = 'Y'
 
 def new_board_main(new_board)
-  puts "The Current Board position looks like this \n"
+  puts 'The Current Board position looks like this:', ''
   puts "|#{new_board.array[0]}|#{new_board.array[1]}|#{new_board.array[2]}|"
   puts "|#{new_board.array[3]}|#{new_board.array[4]}|#{new_board.array[5]}|"
-  puts "|#{new_board.array[6]}|#{new_board.array[7]}|#{new_board.array[8]}| \n"
-end
-# define game rules
-
-def game_rules
-  puts "\n Rules are Follows"
-  puts "1. Game ends when either 3 X's or 3 0's are made one one after the other Horizontally, vertically
-or diagonally"
-  puts '2. Game ends in a draw when all squares are filled or and no winning sequence is reached.'
-  puts "3. Player 1 is X & player 2 is 0 \n"
-  puts "\nLets Begin the Game\n"
+  puts "|#{new_board.array[6]}|#{new_board.array[7]}|#{new_board.array[8]}|"
+  puts
 end
 
 def valid_moves(valid_array, new_board)
-  puts "Valid Moves are as follows:\n"
+  print 'Valid Moves are as follows: '
   x = valid_array.valid_moves_logic(new_board)
   x.each_with_index { |item, index| print "#{index + 1} " if item == 'A' }
   puts
+  puts 'Enter any of the above number to mark your position'
 end
 
 def check_move(move_made, valid_array, new_board)
@@ -36,12 +28,13 @@ def check_move(move_made, valid_array, new_board)
     check = move_made.move_new(move, valid_array)
     next unless check == false
 
-    puts "Invalid Move\n"
+    puts 'Invalid Move', ''
     valid_moves(valid_array, new_board)
-    puts "Enter correct position\n"
+    puts
+    puts 'Enter correct position', ''
     move = gets.chomp.strip
   end
-  puts "You entered in position #{move} \n"
+  puts "You entered in position #{move}"
   move
 end
 
@@ -52,10 +45,11 @@ end
 # player 1 turn
 
 def player_one(valid_array, move_made, new_board, new_player1, result)
-  puts "Your Turn #{new_player1.name}\n"
+  puts "Your Turn #{new_player1.name}", ''
   new_board_main(new_board)
   valid_moves(valid_array, new_board)
-  puts "Enter your position #{new_player1.name}\n"
+  puts
+  puts "Enter your position #{new_player1.name}", ''
   move = check_move(move_made, valid_array, new_board)
   puts
   make_move(move, 'player1', new_board)
@@ -65,10 +59,11 @@ def player_one(valid_array, move_made, new_board, new_player1, result)
 end
 
 def player_two(valid_array, move_made, new_board, new_player2, result)
-  puts "Your Turn #{new_player2.name}\n"
+  puts "Your Turn #{new_player2.name}", ''
   new_board_main(new_board)
   valid_moves(valid_array, new_board)
-  puts "Enter your position #{new_player2.name}\n"
+  puts
+  puts "Enter your position #{new_player2.name}", ''
   move = check_move(move_made, valid_array, new_board)
   puts
   make_move(move, 'player2', new_board)
@@ -81,40 +76,47 @@ def game_over(new_player, final)
   case final
   when 'player1'
     new_player1 = new_player
-    puts " #{new_player1.name} is the winner! Congratulations!!!"
+    puts "#{new_player1.name} is the winner! Congratulations!!!", ''
   when 'player2'
     new_player2 = new_player
-    puts " #{new_player2.name} is the winner! Congratulations!!!"
+    puts "#{new_player2.name} is the winner! Congratulations!!!", ''
   when 'draw'
     puts 'Match is drawn'
   end
-  puts "Thank You for playing\n"
-  puts "Would you like to play again? Press Y to continue\n"
+  puts 'Thank You for playing', ''
+  puts 'Would you like to play again? Press Y to continue, anything else to quit', ''
   play_again = gets.chomp.strip
   exit 1 unless %w[Y y].include?(play_again)
 end
 
+def start_game
+  puts
+  puts 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  puts 'x                              x'
+  puts 'x    Lets Play Tic Tac Toe!    x'
+  puts 'x                              x'
+  puts 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  puts
+end
+
 while %w[Y y].include?(play_again)
-  puts "Lets Play Tic Tac Toe!\n"
   # Get player name
-  puts "Enter Player 1 Name \n"
+  puts
+  puts 'Enter Player 1 Name:'
   player1 = gets.chomp.strip
   player1 = 'Player 1' if player1 == ''
-  puts
   new_player1 = Player.new(player1)
-  puts "Player 1 is #{new_player1.name}\n"
-  puts "Enter Player 2 Name\n"
+  puts
+  puts "Player 1 is \"#{new_player1.name}\"", ''
+  puts 'Enter Player 2 Name:'
   player2 = gets.chomp.strip
-  puts
   player2 = 'Player 2' if player2 == ''
-  puts
   new_player2 = Player.new(player2)
-  puts "Player 2 is #{new_player2.name}"
   puts
+  puts "Player 2 is \"#{new_player2.name}\""
 
   # Initialize array and result
-  puts "Welcome to the Game\n"
-  game_rules
+  start_game
   new_board = Newboard.new
   valid_array = Valid.new
   move_made = Move.new
