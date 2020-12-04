c6567a290d7a00edc11d8b4c2e7440e0a8fbe622
akshay-narkar <akshaynarkar1295@gmail.com>
2020-12-04 17:10:17 +0530

    Change CheckResult logic

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,58 +1,24 @@
 class CheckResult
   attr_reader :result1
+  attr_accessor :points
 
   def initialize(result)
     @result1 = result
+    @win_combinations = [%w[1 2 3], %w[4 5 6], %w[7 8 9], %w[1 4 7], %w[2 5 8], %w[3 6 9], %w[1 5 9], %w[3 5 7]]
+    @points = 0
   end
 
   def check_result(player, new_board)
-    i = 0
-    while i < 7
-      if new_board.array[i] != '-' && new_board.array[i] == new_board.array[i + 1] && \
-         new_board.array[i + 1] == new_board.array[i + 2] && new_board.array[i + 2] == new_board.array[i]
+    @win_combinations.each do |array|
+      array.each do |n|
+        @points += 1 if new_board.array[n.to_i - 1] == 'X'
+        @points -= 1 if new_board.array[n.to_i - 1] == 'O'
+      end
+      if @points == 3 || @points == -3
         @result1 = true
         return player
       end
-      i += 3
-    end
-    check_vertical(player, new_board) if @result1 == false
-  end
-
-  def check_vertical(player, new_board)
-    i = 0
-    while i < 3
-      if new_board.array[i] != '-' && new_board.array[i] == new_board.array[i + 3] && \
-         new_board.array[i + 3] == new_board.array[i + 6] && new_board.array[i + 6] == new_board.array[i]
-        @result1 = true
-        return player
-      end
-      i += 1
-    end
-    check_left_diagonal(player, new_board) if @result1 == false
-  end
-
-  def check_left_diagonal(player, new_board)
-    i = 0
-    while i < 1
-      if new_board.array[i] != '-' && new_board.array[i] == new_board.array[i + 4] && \
-         new_board.array[i + 4] == new_board.array[i + 8] && new_board.array[i + 8] == new_board.array[i]
-        @result1 = true
-        return player
-      end
-      i += 1
-    end
-    check_right_diagonal(player, new_board) if @result1 == false
-  end
-
-  def check_right_diagonal(player, new_board)
-    i = 0
-    while i < 1
-      if new_board.array[i + 2] != '-' && new_board.array[i + 2] == new_board.array[i + 4] && \
-         new_board.array[i + 4] == new_board.array[i + 6] && new_board.array[i + 6] == new_board.array[i + 2]
-        @result1 = true
-        return player
-      end
-      i += 1
+      @points = 0
     end
     check_draw(new_board) if @result1 == false
   end
